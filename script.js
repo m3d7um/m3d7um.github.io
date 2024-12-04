25f35f1831ab86c771226b562a365b459ac67722
m3d7um <andreykastanov2408@gmail.com>
2024-12-04 11:05:22 +0500

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
-window.onload = function() {
+window.addEventListener('load', function() {
     let preloader = document.getElementById('preloader');
     preloader.classList.add('hide-preloader');
     setInterval(function() {
-          preloader.classList.add('preloader-hidden');
+        preloader.classList.add('preloader-hidden');
     }, 990);
-    }
+});
